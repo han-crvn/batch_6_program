35681e99f7dd7093596d357fc572cd866e672a2c
Shane <palomoshanecraven@gmail.com>
2025-03-26 19:44:37 +0800

    Check if it ends with 'er' and print result.

--- without_endswith.py
+++ without_endswith.py
@@ -1,6 +1,8 @@
 # Allow user to input the word or sentence.
 message = input("Enter the word or sentence: ")
 
-# Check if the word or sentence ends with " ".
-
-# Print result.
+# Check if the word or sentence ends with "er" and print the result.
+if message[-2:] == "er":
+    print("It ends with 'er'. ")
+else: 
+    print("It does not ends with 'er'.")
