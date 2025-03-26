d0297329c45dceecb44a6e20915acbdb5921edac
Shane <palomoshanecraven@gmail.com>
2025-03-26 19:19:52 +0800

    Add a new variable to store, change the input to lowercase, and print result.

--- without_lower.py
+++ without_lower.py
@@ -2,5 +2,13 @@
 message = input("Enter the word or sentence: ")
 
 # Change the input to lowercase.
+lower_message = ""
+
+for char in message:
+    if "A" <= char <= "Z":
+        lower_message += chr(ord(char) + 32)
+    else:
+        lower_message += char
 
 # Print the result.
+print(lower_message)
