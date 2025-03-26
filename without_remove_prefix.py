b191ec6885a7408e78610c169aaa5739e0b8a399
Shane <palomoshanecraven@gmail.com>
2025-03-26 19:12:38 +0800

    Remove the specific prefix and print result.

--- without_remove_prefix.py
+++ without_remove_prefix.py
@@ -1,6 +1,9 @@
 # Allow user to input the word or sentence.
-message = input("Enter the word or sentence: ")
+message = input("Enter the word or sentence: ").lower()
 
-# Remove the prefix.
+# Remove the prefix (the prefix in this program is 'mis').
+if message.startswith("mis"):
+    message = message[3:]
 
 # Print the result.
+print(message)
