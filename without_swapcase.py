b1ddcbe2f4dc6c85af53196920e322a0ff9a51ed
Shane <palomoshanecraven@gmail.com>
2025-03-26 20:23:32 +0800

    Reverse the uppercase and lowercase and print the result.

--- without_swapcase.py
+++ without_swapcase.py
@@ -2,5 +2,15 @@
 message = input("Enter the word or sentence: ")
 
 # Change uppercase to lowercase and lowercase to uppercase.
+reverse_message = ""
+
+for char in message:
+    if "A" <= char <= "Z":
+        reverse_message += chr(ord(char) + 32)
+    elif "a" <= char <= "z":
+        reverse_message += chr(ord(char) - 32)
+    else:
+        reverse_message += char 
 
 # Print the result.
+print(reverse_message)
