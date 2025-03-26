b979f71456265e3af569b5364802698e5463b0ac
Shane <palomoshanecraven@gmail.com>
2025-03-26 19:34:23 +0800

    Check if all uppercase or not and print the result.

--- without_isupper.py
+++ without_isupper.py
@@ -2,5 +2,12 @@
 message = input("Enter the word or sentence: ")
 
 # Check if all letters is uppercase
+for char in message:
+    if "A" <= char <= "Z":
+        result = "All uppercase."
+    else:
+        result = "There is a lowercase letter/s."
+        break
 
 # Print result
+print(result)
