98f541d94fcc221760855e12922dc46d27a66f65
Shane <palomoshanecraven@gmail.com>
2025-03-26 20:14:32 +0800

    Add total width of line,  adjust the left and right padding, and print result.

--- without_center.py
+++ without_center.py
@@ -2,7 +2,13 @@
 message = input("Enter the word or sentence: ")
 
 # Add total width of line and adjust left and right padding
+width = 176
+
+left_pad = (width - len(message)) // 2
+right_pad = width - len(message) - left_pad
 
 # Add the padding
+center_message = " " * left_pad + message + " " * right_pad
 
 # Print the result
+print(center_message)
