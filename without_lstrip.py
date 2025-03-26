a14a1453c49bf1aa299b3d16e708fe539a876f66
Shane <palomoshanecraven@gmail.com>
2025-03-26 19:02:09 +0800

    Remove left padded spaces and print the result.

--- without_lstrip.py
+++ without_lstrip.py
@@ -2,5 +2,10 @@
 message = input("Enter the word or sentence: ")
 
 # Remove the spaces from left.
+for i in range (len(message)):
+    if message[i] != " ":
+        message = message[i:]
+        break
 
 # Print the result.
+print(message)
