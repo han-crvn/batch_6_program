049abae7f2d894f4afaff4f8ec37f6e77a3d843a
Shane <palomoshanecraven@gmail.com>
2025-03-27 15:13:14 +0800

    Change the input to capitalize format and print the result.

--- without_capitalize.py
+++ without_capitalize.py
@@ -2,5 +2,7 @@
 message = input("Enter the word or sentence: ")
 
 # Change the first letter of the word or sentence to uppercase and the remaining to lowercase.
+capitalize_message = message[0].upper() + message[1:].lower()
 
-# Print result.
+# Print the result.
+print(capitalize_message)
