518de78a605c222c3504dfc2715da9492ac3d432
Shane <palomoshanecraven@gmail.com>
2025-03-27 15:06:31 +0800

    Allow users to input.

---
+++ without_capitalize.py
@@ -0,0 +1,6 @@
+# Allow user to input the word or sentence.
+message = input("Enter the word or sentence: ")
+
+# Change the first letter of the word or sentence to uppercase and the remaining to lowercase.
+
+# Print result.
