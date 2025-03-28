4db0cf5ab73d0fa4fd0607a265d78e9d87f89b96
Shane <palomoshanecraven@gmail.com>
2025-03-28 20:33:40 +0800

    Allow users to input multiple lines and print result.

--- without_ljust.py
+++ without_ljust.py
@@ -1,9 +1,19 @@
-# Allow user to input the word or sentence.
-message = input("Enter the word or sentence: ")
+# Add store for lines of messages
+line_of_message = []
 
-# Format the input to left justify.
-spaces = 50 - len(message)
-message = message + " " * spaces
+# Allow user to input the word or sentence.
+while True:
+    message = input("Enter the word or sentence (enter a digit to stop): ")
+    
+    # Inputting number will break if a number is inputted.
+    if message.isdigit():
+        break
+
+    else:
+
+        # Format the input to left justify.
+        line_of_message.append(message + " " * ( 50 - len(message)))
 
 # Print the result.
-print(message)
+for message in line_of_message:
+    print(message)
