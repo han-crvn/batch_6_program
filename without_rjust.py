b99d710f68e200c3b1964dce0a7f2deda0beebd1
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:43:38 +0800

    Allow users to input and stop inputting.

--- without_rjust.py
+++ without_rjust.py
@@ -1,8 +1,13 @@
 # Add storage for lines of messages
+line_of_message = []
 
 # Allow user to input the word or sentence.
-
-# Inputting number will break if a number is inputted.
+while True:
+    message = input("Enter the word or sentence (enter a digit to stop): ")
+    
+    # Inputting number will break if a number is inputted.
+    if message.isdigit():
+        break
 
 # Format the input to right justify.
         
