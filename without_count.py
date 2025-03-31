80aaa0bb497d042cd8c6daf029117f9b72c4948f
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:24:59 +0800

    Find the number of appearance of certain character and print result.

--- without_count.py
+++ without_count.py
@@ -2,8 +2,13 @@
 message = input("Enter the word or sentence: ")
 
 # Allow users to choose what character to count.
-find_char = input("Enter a chracter to find: ")
+find_char = input("Enter a character to find: ")
 
 # Count the numbers of chosen character.
+count = 0
+for char in message:
+    if char == find_char:
+        count += 1
 
 # Print result.
+print(f"{find_char}: {count}")
