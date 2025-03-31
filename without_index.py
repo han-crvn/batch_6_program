9b08fdd0d9c2505e1728a74575979ac34a4d99a4
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:28:20 +0800

    find the location of the first appearance of the char and print result.

--- without_index.py
+++ without_index.py
@@ -5,5 +5,15 @@ message = input("Enter the word or sentence: ")
 locate_char = input("Enter a character to find: ")
 
 # Find the first index of chosen character.
+position = -1
+
+for i in range(len(message)):
+    if message[i] == locate_char :
+        position = i
+        break
 
 # Print result.
+if position != -1:
+    print(f"{locate_char}: at index ({position})")
+else:
+    print("Not found.")
