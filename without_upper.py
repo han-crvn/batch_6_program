25e1d6bbc55b4b3a7a67bcd6b4ee80e56e33880c
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:51:27 +0800

    Format to uppercase and print the result.

--- without_upper.py
+++ without_upper.py
@@ -2,5 +2,13 @@
 message = input("Enter the word or sentence: ")
 
 # Format the input to uppercase.
+upper_message = ""
+
+for char in message:
+    if "a" <= char <= "z":
+        upper_message += chr(ord(char) - 32)
+    else:
+        upper_message += char
 
 # Print the result.
+print(upper_message)
