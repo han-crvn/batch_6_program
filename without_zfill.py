aa2e1d4931b2431f95e415bb459bf2a005b047a6
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:54:11 +0800

    Format the input and print result.

--- without_zfill.py
+++ without_zfill.py
@@ -2,6 +2,13 @@
 numbers = input("Enter a number from range 1-999999: ")
 
 # Format the input.
+if 1 <= int(numbers) <= 999999:
+    zero_fill = 6 - len(numbers)
+    formatted_number = "0" * zero_fill + numbers
 
-# Print result.
+    # Print result.
+    print(formatted_number)
+
+else:
+    print("Number must be from range 1-999999.")
     
