b06202f1f9b62f42701d8d83a5608712ad4230ef
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:46:53 +0800

    remove right spaces and print the result.

--- without_rstrip.py
+++ without_rstrip.py
@@ -2,5 +2,10 @@
 message = input("Enter the word or sentence: ")
 
 # Remove the spaces from the right side.
+for i in range(len(message) - 1, -1, -1):
+    if message[i] != " ":
+        message = message[:i + 1]
+        break
 
 # Print the result.
+print(message)
