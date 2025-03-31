82ff2f607a4d748d661de1a821131bc157ae5ce0
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:49:12 +0800

    Check if starts with 'dis' and print result.

--- without_startswith.py
+++ without_startswith.py
@@ -2,3 +2,7 @@
 message = input("Enter the word or sentence: ")
 
 # Check if the word or sentence starts with "dis" and print the result.
+if message[:3] == "dis":
+    print("It starts with 'dis'. ")
+else: 
+    print("It does not starts with 'dis'.")
