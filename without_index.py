fc03282ec23c0a61b04c786806edf26db75dc48e
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:27:46 +0800

    Allow users to input message and find the index of certain character.

--- without_index.py
+++ without_index.py
@@ -1,6 +1,8 @@
 # Allow users to input the word or sentence.
+message = input("Enter the word or sentence: ")
 
 # Allow users to choose what character to find.
+locate_char = input("Enter a character to find: ")
 
 # Find the first index of chosen character.
 
