89ab1aa2f00e1392fc023f7e5b209d66eae4da78
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:23:51 +0800

    Allow users to input.

--- without_count.py
+++ without_count.py
@@ -1,6 +1,8 @@
 # Allow users to input the word or sentence.
+message = input("Enter the word or sentence: ")
 
 # Allow users to choose what character to count.
+find_char = input("Enter a chracter to find: ")
 
 # Count the numbers of chosen character.
 
