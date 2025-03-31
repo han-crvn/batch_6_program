a9c1d430d0fcbe74d131297fd0132ad892321612
Shane <palomoshanecraven@gmail.com>
2025-04-01 01:38:06 +0800

    Pseudocode for this program.

---
+++ without_rjust.py
@@ -0,0 +1,9 @@
+# Add storage for lines of messages
+
+# Allow user to input the word or sentence.
+
+# Inputting number will break if a number is inputted.
+
+# Format the input to right justify.
+        
+# Print the result.
